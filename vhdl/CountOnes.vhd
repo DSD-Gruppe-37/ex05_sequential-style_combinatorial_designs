b492614d29dfaedcc1d8da286002914424979fd7
AdamHoej-au <au617583@post.au.dk>
2020-04-19 15:04:48 +0200

    Count ones done

--- vhdl/CountOnes.vhd
+++ vhdl/CountOnes.vhd
@@ -1,34 +1,47 @@
--- LIBRARY IEEE;
--- USE IEEE.std_logic_1164.ALL;
--- USE IEEE.numeric_std.ALL;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
 
--- ENTITY CountOnes IS
---     PORT
---     (
---         in1  : IN std_logic_vector(7 DOWNTO 0);
---         out1 : OUT std_logic_vector(6 DOWNTO 0)
---     );
--- END ENTITY CountOnes;
--- ARCHITECTURE Counts OF CountOnes IS
+ENTITY CountOnes IS
+    PORT
+    (
+        A    : IN std_logic_vector(7 DOWNTO 0);
+        ones : OUT std_logic_vector(3 DOWNTO 0)
+    );
+END ENTITY CountOnes;
+ARCHITECTURE Count OF CountOnes IS
+BEGIN
+    Counter : PROCESS (A)
+        VARIABLE OneCounter : INTEGER;
+    BEGIN
+        OneCounter := 0;
+        FOR I IN 0 TO 7 LOOP
+            IF a(i) = '1' THEN
+                OneCounter := OneCounter + 1;
+            END IF;
+        END LOOP;
+        ones <= std_logic_vector(to_unsigned(OneCounter,4));
+    END PROCESS Counter;
+END ARCHITECTURE Count;
 
 --     SIGNAL CountOutput : std_logic_vector(3 DOWNTO 0);
 -- BEGIN
---     CountOne : PROCESS (in1)
+--     CountOne : PROCESS (A)
 --         VARIABLE counter : INTEGER;
 --     BEGIN
 --     counter := 0;
 --     -- FOR I IN 0 TO 7 LOOP
---     --     counter := counter + to_integer(unsigned(in1(I)));
+--     --     counter := counter + to_integer(unsigned(A(I)));
 
 --     -- END LOOP;
 --     -- CountOutput <= std_logic_vector(to_unsigned(counter, 4));
 
 --     for I in 0 TO 7 loop
---         if in1(I) = "1" then
+--         if A(I) = "1" then
 --             counter := (counter + 1);
 --         end if;
 --     end loop;
-    
+
 --     END PROCESS CountOne;
 
 --     Hexdisplay : ENTITY bin2hex(Behavioral)
@@ -37,6 +50,5 @@
 --             -- INPUTS
 --             bin(3 DOWNTO 0)  => CountOutput(3 DOWNTO 0),
 --             -- OUTPUTS
---             Sseg(6 DOWNTO 0) => out1(6 DOWNTO 0)
+--             Sseg(6 DOWNTO 0) => ones(6 DOWNTO 0)
 --         );
--- END ARCHITECTURE Counts;
---
+++ vhdl/countonestester.vhd
@@ -0,0 +1,36 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
+USE work.ALL;
+
+ENTITY countOnesTester IS
+    PORT
+    (
+        a_in   : IN std_logic_vector(7 DOWNTO 0);
+        hexout : OUT std_logic_vector(6 DOWNTO 0)
+    );
+END ENTITY countOnesTester;
+
+ARCHITECTURE rtl OF countOnesTester IS
+
+    SIGNAL OnesCount : std_logic_vector(3 DOWNTO 0);
+
+BEGIN
+
+    CountOne : ENTITY CountOnes
+        PORT MAP
+        (
+            A    => a_in,
+            ones => OnesCount
+        );
+
+    Hexdisplay : ENTITY bin2hex(Behavioral)
+        PORT
+        MAP
+        (
+        -- INPUTS
+        bin  => OnesCount,
+        -- OUTPUTS
+        Sseg => hexout
+        );
+END ARCHITECTURE rtl;
